5a444b143cd8cb81135e7e8efde920a720222023
David Page <d.c.page@gmail.com>
2018-02-19 16:40:14 +0000

    restrict returns a subgraph

--- gamma/core.py
+++ gamma/core.py
@@ -63,7 +63,7 @@ def walk_nodes(neighbours, starting_nodes):
 def restrict(graph, inputs, outputs):
     neighbours = lambda node: (n for n in get_inputs(graph[node])
                                if (node in graph and n not in inputs))
-    return walk_nodes(neighbours, outputs)
+    return subgraph(graph, set(walk_nodes(neighbours, outputs)))
 
 
 def edges(graph):
