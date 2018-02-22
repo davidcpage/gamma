d2cd91ef89ac4577baede6a8c431d8b614321763
David Page <d.c.page@gmail.com>
2018-02-22 12:55:43 +0000

    add .protobuf to unwrap protobufs and use this in .convert

--- gamma/convert.py
+++ gamma/convert.py
@@ -1,46 +1,38 @@
 import numpy as np
-from google.protobuf.json_format import MessageToDict, ParseDict
-from .core import union, reindex
+from google.protobuf.json_format import ParseDict, MessageToDict
+from .core import reindex
 import onnx
 from onnx import numpy_helper
-
-def from_onnx_nodes(nodes):
-  to_np = lambda a: numpy_helper.to_array(a) if isinstance(a, onnx.TensorProto) else a
-  return {k: ({
-    'label': k,
-    'params': {a.name: to_np(onnx.helper.get_attribute_value(a)) for a in n.attribute},
-    'type': n.op_type
-  }, list(n.input)) for n in nodes for k in n.output}
+from .protobuf import unwrap
+from itertools import chain
 
 def from_onnx(onnx_model):
-    net = from_onnx_nodes(onnx_model.graph.node)
-    constants = from_onnx_nodes([onnx.helper.make_node('Constant', inputs=[], 
-                       outputs=[x.name], value=x) for x in onnx_model.graph.initializer])
-    inputs = {x.name: ({'type': 'Input', 'label': x.name, 'params': MessageToDict(x)}, [])
-              for x in onnx_model.graph.input}
-    graph = union(inputs, constants, net)
-    return reindex(graph, {k: i for (i, k) in enumerate(graph.keys())})
-  
+    g = unwrap(onnx_model.graph)
+    ext_inputs = ((label, params, 'Input', []) for (label, params) in g.get('input',[]))
+    constants =  ((label, {'value': v}, 'Constant', []) for (label, v) in g.get('initializer',[]))
+    net =        ((label, dict(n.get('attribute',())), n['op_type'], n.get('input', [])) 
+                   for n in g['node'] for label in n['output'])
+    return {label: ({'label': label, 'params': params, 'type': type}, inputs) 
+            for (label, params, type, inputs) in chain(ext_inputs, constants, net)}
+    
+
 def to_onnx(graph, name='', initializer=None):
-  from_np = lambda a: numpy_helper.from_array(a) if isinstance(a, np.ndarray) else a
-  nodes = [onnx.helper.make_node(attr['type'], [str(i) for i in inputs], [str(n)],
-                                 label=attr['label'],
+    from_np = lambda a: numpy_helper.from_array(a) if isinstance(a, np.ndarray) else a
+    nodes = [onnx.helper.make_node(attr['type'], [str(i) for i in inputs], [str(n)],
+                                 #label=attr['label'],
                                  **{k: from_np(v) for (k,v) in attr['params'].items()})
            for (n, (attr, inputs)) in graph.items()
            if attr['type'] != 'Input']
-  inputs = [ParseDict(a['params'], onnx.ValueInfoProto())
+    inputs = [ParseDict(a['params'], onnx.ValueInfoProto())
             for (a,_) in graph.values() if a['type'] == 'Input']
-  outputs = []
-  onnx_graph = onnx.helper.make_graph(nodes, name, inputs, outputs, initializer=initializer)
-  return onnx.helper.make_model(onnx_graph)
+    outputs = []
+    onnx_graph = onnx.helper.make_graph(nodes, name, inputs, outputs, initializer=initializer)
+    return onnx.helper.make_model(onnx_graph)
 
 def from_tflow(graph_def):
-    import tensorflow as tf
-    unwrap = lambda arg: tf.make_ndarray(arg.tensor) if arg.HasField('tensor') else MessageToDict(arg)
-    graph = {n.name: ({'type': n.op, 'label': n.name, 'params':
-                       {k: unwrap(v) for k, v in n.attr.items()}
-                       }, [i.split('^', 1)[-1].split(':', 1)[0] for i in n.input])
-             for n in graph_def.node}
+    graph = {n['name']: ({'type': n['op'], 'label': n['name'], 'params': n.get('attr',{})}, 
+                     [i.split('^', 1)[-1].split(':', 1)[0] for i in n.get('input', [])])
+             for n in unwrap(graph_def.node)}   
     return reindex(graph, {k: i for (i, k) in enumerate(graph.keys())})
 
 
---
+++ gamma/protobuf.py
@@ -0,0 +1,65 @@
+from functools import singledispatch
+
+from google.protobuf.descriptor import FieldDescriptor
+from google.protobuf.internal.containers import MessageMap, RepeatedCompositeFieldContainer
+from google.protobuf.json_format import MessageToDict
+
+import tensorflow as tf
+from tensorflow.core.framework import tensor_pb2, tensor_shape_pb2
+
+import onnx
+
+def identity(x):
+    return x
+
+def unwrap_standard(pb):
+    return {f.name: unwrap(v) for (f, v) in pb.ListFields()}
+
+def enum_to_string(field, value):
+    if field.label == FieldDescriptor.LABEL_REPEATED:
+        return [field.enum_type.values_by_number[v].name for v in value] 
+    return field.enum_type.values_by_number[value].name    
+
+unwrap_containers = {
+    MessageMap: lambda pb: {k: unwrap(v) for k,v in pb.items()},
+    RepeatedCompositeFieldContainer: lambda pb: [unwrap(v) for v in pb],
+}
+
+################
+## tensorflow
+###############
+
+
+def unwrap_tf_AttrValue(pb):
+    [(field, value)] = pb.ListFields()
+    return enum_to_string(field, value) if field.type == FieldDescriptor.TYPE_ENUM else unwrap(value)
+
+unwrap_tf = {
+    tf.NodeDef: unwrap_standard,
+    tf.GraphDef: unwrap_standard,
+    tf.AttrValue: unwrap_tf_AttrValue,
+    tf.AttrValue.ListValue: unwrap_tf_AttrValue,
+    tensor_pb2.TensorProto: tf.make_ndarray,
+    tensor_shape_pb2.TensorShapeProto: lambda pb: [x.size for x in pb.dim]
+}
+
+#############
+## onnx
+#############
+
+
+unwrap_onnx = {
+    onnx.ModelProto: unwrap_standard,
+    onnx.NodeProto: unwrap_standard,
+    onnx.GraphProto: unwrap_standard,
+    onnx.ValueInfoProto: lambda pb: (pb.name, MessageToDict(pb)), #too lazy to drill deeper into this for now
+    #onnx.TypeProto: unwrap_standard,
+    #onnx.TypeProto.Tensor: unwrap_standard,
+    onnx.AttributeProto: lambda pb: (pb.name, unwrap(onnx.helper.get_attribute_value(pb))),
+    onnx.TensorProto: lambda pb: (pb.name, onnx.numpy_helper.to_array(pb)),
+}
+
+unwrap = singledispatch(identity) #default is to do no unwrapping, making it easier to explore
+for unwrappers in (unwrap_containers, unwrap_tf, unwrap_onnx):
+    for (type_, func) in unwrappers.items():
+        unwrap.register(type_, func)
