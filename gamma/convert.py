17e6755703704c695d8e59ecc13009d2172985f2
Peter Baldwin <peter@myrtlesoftware.com>
2018-02-28 14:48:39 +0000

    to_onnx: put nodes in topological order

--- gamma/convert.py
+++ gamma/convert.py
@@ -1,14 +1,28 @@
 import numpy as np
 from google.protobuf.json_format import ParseDict, MessageToDict
-from .core import reindex
+from .core import reindex, out_edges
 import onnx
 from onnx import numpy_helper
 from .protobuf import unwrap
 from itertools import chain
+from collections import OrderedDict
 
 def make_tensor_value_info(name, elem_type, *args, **kwargs):
     elem_type = onnx.TensorProto.DESCRIPTOR.enum_types_by_name['DataType'].values_by_name[elem_type].number
     return onnx.helper.make_tensor_value_info(name, elem_type, *args, **kwargs)
+
+class TopoGraph(OrderedDict):
+    def __init__(self, graph):
+        self.graph = graph
+        edges = out_edges(graph)
+        for k in graph:
+            if len(edges.get(k, [])) == 0:
+                self[k]
+    def __missing__(self, k):
+        (a,i) = self.graph[k]
+        [self[v] for v in i]
+        self[k] = (a,i)
+        return self[k]
 
 def from_onnx(onnx_model):
     g = unwrap(onnx_model.graph)
@@ -20,6 +34,7 @@ def from_onnx(onnx_model):
             for (label, params, type, inputs) in chain(ext_inputs, constants, net)}
 
 def to_onnx(graph, name, outputs=[], initializer=None):
+    graph = TopoGraph(graph)
     from_np = lambda a: numpy_helper.from_array(a) if isinstance(a, np.ndarray) else a
     nodes = [onnx.helper.make_node(attr['type'], [str(i) for i in inputs], [str(n)],
                                    **{k: from_np(v) for (k,v) in attr['params'].items()})
@@ -29,7 +44,7 @@ def to_onnx(graph, name, outputs=[], initializer=None):
     outputs = [make_tensor_value_info(str(n), **a)
                for (n,a) in outputs]
     onnx_graph = onnx.helper.make_graph(nodes, name, inputs, outputs, initializer=initializer)
-    return onnx.helper.make_model(onnx_graph)
+    return onnx.helper.make_model(onnx_graph, producer_name='Gamma')
 
 def from_tflow(graph_def):
     graph = {n['name']: ({'type': n['op'], 'label': n['name'], 'params': n.get('attr',{})}, 
