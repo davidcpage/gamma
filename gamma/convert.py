434d32efcd86b19497bb503eabbb0dd2c92b7984
David Page <d.c.page@gmail.com>
2018-05-14 17:02:03 +0100

    onnx expects a topologically sorted graph

--- gamma/convert.py
+++ gamma/convert.py
@@ -3,7 +3,7 @@ import numpy as np
 from google.protobuf.json_format import ParseDict, MessageToDict
 import onnx
 from onnx import numpy_helper
-from .core import reindex, make_node_attr, path_iter
+from .core import reindex, make_node_attr, path_iter, topological_sort
 from .protobuf import unwrap
 
 def make_tensor_value_info(name, elem_type, *args, **kwargs):
@@ -21,12 +21,13 @@ def from_onnx(onnx_model):
 
 
 def to_onnx(graph, name, outputs=None, initializer=None):
+    graph = list(topological_sort(graph))
     from_np = lambda a: numpy_helper.from_array(a) if isinstance(a, np.ndarray) else a
     nodes = [onnx.helper.make_node(attr['type'], [str(i) for i in attr['inputs']], [str(n)],
                                    **{k: from_np(v) for (k,v) in attr['params'].items()})
-             for (n, attr) in graph.items() if attr['type'] != 'Input']
+             for (n, attr) in graph if attr['type'] != 'Input']
     inputs = [make_tensor_value_info(str(n), **a['params'])
-              for (n, a) in graph.items() if a['type'] == 'Input']
+              for (n, a) in graph if a['type'] == 'Input']
     outputs = [] if outputs is None else [make_tensor_value_info(str(n), **a)
                for (n, a) in outputs]
     onnx_graph = onnx.helper.make_graph(nodes, name, inputs, outputs, initializer=initializer)
