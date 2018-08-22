c2f97a58d98b447b04baa2ed57c678fcc8834188
David Page <d.c.page@gmail.com>
2018-08-22 13:51:38 +0100

    display graphs in jupyterlab

--- gamma/utils.py
+++ gamma/utils.py
@@ -1,5 +1,5 @@
 import pydot
-from IPython.display import display, HTML
+from IPython.display import display, HTML, SVG
 from gamma.core import FuncCache, input_nodes, depths, path_iter
 import os
 from urllib.request import urlretrieve
@@ -71,12 +71,10 @@ def draw(graphs, legend=True, scale=1, sep='/', **kwargs):
         edges = ((src, k, {}) for k, n in graph.items()
                 for src in input_nodes(n))
         svg = draw_pydot(nodes, edges, size=size, **kwargs)
-        width = int(re.search('width="([0-9]*)pt"',svg).groups()[0])
-        html += '<div style="min-width: {width}pt">{svg}</div>'.format(width=width, svg=svg)
         types += [a['type'] for (a, i) in graph.values()]
     if legend:
-        html = ColorMap.html({t: COLORS[t] for t in types}) + html
-    display(HTML(html))
+        display(HTML(ColorMap.html({t: COLORS[t] for t in types})))
+    display(SVG(svg))
 
 
 def draw_pydot(nodes, edges, direction='LR', **kwargs):
